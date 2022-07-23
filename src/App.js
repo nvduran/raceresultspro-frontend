2713121642b9ba691327d4d96fb61ce3a8730bd8
nvduran <nvduran@gmail.com>
2022-07-23 16:23:53 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <div className="App">123</div>;
+}
+
+export default App;
