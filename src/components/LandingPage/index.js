20394cece4a8f133937542cfd7e54a4c080e55d7
nvduran <nvduran@gmail.com>
2022-07-26 20:57:42 -0500

    landing page major styling overhaul

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -11,16 +11,27 @@ import {
   Form,
 } from "react-bootstrap";
 import "../../styles/LandingPage.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClipboard } from "@fortawesome/free-regular-svg-icons";
+import { faCarBurst } from "@fortawesome/free-solid-svg-icons";
 
 export default function LandingPage() {
   return (
     <div>
       <Container className="landing-container">
         <Row className="iracing-stats-row">
-          <Col className="iracing-stats-col">iRacing Stats here</Col>
+          <Col className="iracing-stats-left">
+            <FontAwesomeIcon icon={faClipboard} size="2x" />
+            iRating Stats
+          </Col>
+          <Col className="iracing-stats-col"></Col>
         </Row>
         <Row className="incident-stats-row">
-          <Col className="incident-stats-col">Incident Stats here</Col>
+          <Col className="incident-stats-left">
+            <FontAwesomeIcon icon={faCarBurst} size="2x" />
+            Incident Stats
+          </Col>
+          <Col className="incident-stats-col"></Col>
         </Row>
       </Container>
     </div>
