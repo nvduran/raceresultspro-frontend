bd8f1d6e9a3c4c37454fb3e00200ab77637c28e5
nvduran <nvduran@gmail.com>
2022-07-27 21:48:57 -0500

    mobile sizing

--- src/components/iRatingStats/index.js
+++ src/components/iRatingStats/index.js
@@ -12,11 +12,25 @@ import {
 } from "react-bootstrap";
 import IRatingGraph from "./irating-graph";
 
-export default function iRatingStats() {
-  return (
-    <div>
-      123
-      <IRatingGraph />
-    </div>
-  );
+export default function IRatingStats() {
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 800);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 800);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
+
+  if (!isDesktop) {
+    return <div>MOBILE GRAPH</div>;
+  } else {
+    return (
+      <div>
+        <IRatingGraph />
+      </div>
+    );
+  }
 }
