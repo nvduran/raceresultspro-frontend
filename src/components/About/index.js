4f24f22a860067753417f5620f12edfbeb634f58
nvduran <nvduran@gmail.com>
2022-08-07 21:01:11 -0500

    about mvp

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,16 +8,35 @@ import {
   Nav,
   Row,
   Col,
+  Link,
   Form,
 } from "react-bootstrap";
-import "../../styles/NavBar.css";
+import "../../styles/About.css";
 
 export default function About() {
   return (
     <div>
-      <Row>
-        <Col>asdfasdfasdfasdf</Col>
-      </Row>
+      <Container className="about-backdrop">
+        <Row>
+          <Col className="ar1">
+            Hi, I'm Nick and I'm a web developer in Austin, TX.
+          </Col>
+        </Row>
+        <Row>
+          <Col className="ar2">
+            You can find ways to contact me on{" "}
+            <a href="https://github.com/nvduran" rel="noreferrer">
+              Github
+            </a>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="ar3">
+            This site was made using ReactJS, MongoDB, Express, and NodeJS. This
+            site collects its data from the official iRacing data API
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
