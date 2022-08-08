ab5a7a5fc71da633cf0d4205898f37e474c95c96
nvduran <nvduran@gmail.com>
2022-08-07 22:19:34 -0500

    landing page linking

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -17,25 +17,37 @@ import { faCarBurst } from "@fortawesome/free-solid-svg-icons";
 import { faUsersLine } from "@fortawesome/free-solid-svg-icons";
 
 export default function LandingPage() {
+  const handlePick = (x) => {
+    window.location.href = "/" + x;
+  };
   return (
     <div className="landing-background-div">
       <Container className="landing-container">
         <Row className="hero-text-row">iRacing Stats, all in one place.</Row>
-        <Row className="iracing-stats-row">
+        <Row
+          className="iracing-stats-row"
+          onClick={() => handlePick("iratingstats")}
+        >
           <Col className="iracing-stats-left">
             <FontAwesomeIcon icon={faClipboard} size="2x" />
             <p>iRating Stats</p>
           </Col>
           <Col className="iracing-stats-col"></Col>
         </Row>
-        <Row className="incident-stats-row">
+        <Row
+          className="incident-stats-row"
+          onClick={() => handlePick("incidentstats")}
+        >
           <Col className="incident-stats-left">
             <FontAwesomeIcon icon={faCarBurst} size="2x" />
             <p>Incident Stats</p>
           </Col>
           <Col className="incident-stats-col"></Col>
         </Row>
-        <Row className="member-stats-row">
+        <Row
+          className="member-stats-row"
+          onClick={() => handlePick("searchmember")}
+        >
           <Col className="member-stats-left">
             <FontAwesomeIcon icon={faUsersLine} size="2x" />
             <p>Member Stats</p>
